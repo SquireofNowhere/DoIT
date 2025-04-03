3cc6d1fe3d6a6d0ef61ee718b893f123ca8ff843
forNowhere <140905424+SquireofNowhere@users.noreply.github.com>
2025-04-03 16:39:31 +0200

    second tutorial. incomplete
    I did some stuff and it didnt work out. gonna go figure it out later.
    
    Color of task text not working... or no text at all who knows.
    
    add note button still got black icon

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 To Do List App Android Studio Tutorial by Penguin Coders on youtube
+
+## Errors
+- eyo why tf is the text not showing up for the task??? changed the color cause it thought it was set to white but nah
+    shits just broken. ugh going home
--- app/src/main/java/com/example/doit/MainActivity.java
+++ app/src/main/java/com/example/doit/MainActivity.java
@@ -7,18 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.doit.Adapter.ToDoAdapter;
+import com.example.doit.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter taskAdapter;
+
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        taskAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(taskAdapter);
+
+        ToDoModel task = new ToDoModel();
+
+        task.setTask("This is a dummy task #" + 1);
+        task.setStatus(0);
+        task.setId(0);
+        taskList.add(task);
+
+/*
+        for (int i = 1; i <= 5; i++) {
+            task.setTask("This is a dummy task #" + i);
+            if (i%2 == 0)
+                task.setStatus(0);
+            else task.setStatus(2);
+            task.setId(i);
+            taskList.add(task);
+        } */
+
+        taskAdapter.setTasks(taskList);
+
+
     }
 }
